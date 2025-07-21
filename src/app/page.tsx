45dc8a780eb3b3dcdb8aaf0a5b6c47dd7cfb53a1
Alexandru-Dumitrel-Gheorghe <gheorghe.93@icloud.com>
2025-07-21 20:23:30 +0200

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero/Hero";
 import AboutMe from "@/components/About/About";
+import Services from "@/components/Services/Services";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
       <AboutMe />
+      <Services />
     </main>
   );
 }
